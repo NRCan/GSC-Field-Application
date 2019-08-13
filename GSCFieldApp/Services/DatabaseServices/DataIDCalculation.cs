7ea4032ace232530a0b650386b008f6565424031
Gabriel Huot-Vézina <gabriel.huot-vezina@canada.ca>
2019-08-13 16:58:23 -0400

    #52 Full GUID instead of Small GUID is now implemented for all

--- GSCFieldApp/Services/DatabaseServices/DataIDCalculation.cs
+++ GSCFieldApp/Services/DatabaseServices/DataIDCalculation.cs
@@ -42,7 +42,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateLocationID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -75,7 +75,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateMetadataID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -84,7 +84,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateTermID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
         #endregion
 
@@ -95,7 +95,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateStationID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -326,7 +326,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateEarthmatID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         #endregion
@@ -398,7 +398,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateSampleID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         #endregion
@@ -410,7 +410,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateDocumentID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -497,7 +497,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateStructureID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -567,7 +567,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculatePFlowID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -637,7 +637,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateFossilID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         /// <summary>
@@ -765,7 +765,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateMineralID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
 
         #endregion
@@ -839,7 +839,7 @@ namespace GSCFieldApp.Services.DatabaseServices
         /// <returns></returns>
         public string CalculateMineralAlterationID()
         {
-            return CalculateSmallGUID();
+            return CalculateGUID();
         }
         #endregion
 
@@ -940,14 +940,23 @@ namespace GSCFieldApp.Services.DatabaseServices
 
         }
 
+        ///// <summary>
+        ///// Will calculate a new GUID converted to a base 64 string, hence the small guid.
+        ///// </summary>
+        ///// <returns></returns>
+        //public string CalculateSmallGUID()
+        //{
+        //    return Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("===", "");
+
+        //}
+
         /// <summary>
-        /// Will calculate a new GUID converted to a base 64 string, hence the small guid.
+        /// Will calculate a new GUID
         /// </summary>
         /// <returns></returns>
-        public string CalculateSmallGUID()
+        public string CalculateGUID()
         {
-            return Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Replace("===", "");
-
+            return Guid.NewGuid().ToString();
         }
         #endregion
     }
