cd878df02e4605242086e5bbe28a5dcdf3fa8aaa
gabriel.huot-vezina <gabriel.huot-vezina@canada.ca>
2021-07-08 15:53:42 -0400

    Wrong value for sql where clause to get struc vocab.

--- GSCFieldApp/Views/StructureDialog.xaml.cs
+++ GSCFieldApp/Views/StructureDialog.xaml.cs
@@ -419,7 +419,7 @@ namespace GSCFieldApp.Views
         {
             Vocabularies vocabularyModel = new Vocabularies();
             string vocQuerySelect = "SELECT * FROM " + Dictionaries.DatabaseLiterals.TableDictionary;
-            string vocQueryWhere = " WHERE CODETHEME = 'DETAIL'";
+            string vocQueryWhere = " WHERE CODETHEME = 'STRUCDETAIL'";
             string vocQueryVisibility = " AND " + Dictionaries.DatabaseLiterals.TableDictionary + "." + Dictionaries.DatabaseLiterals.FieldDictionaryVisible + " = '" + Dictionaries.DatabaseLiterals.boolYes + "'";
             string vocFinalQuery = vocQuerySelect + vocQueryWhere + vocQueryVisibility;
 
