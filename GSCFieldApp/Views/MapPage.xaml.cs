9ae42cf31182e6505cce1737a8950848a231c5d4
gabriel.huot-vézina <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2023-07-14 13:59:46 -0400

    Added GPS location sample.

--- GSCFieldApp/Views/MapPage.xaml.cs
+++ GSCFieldApp/Views/MapPage.xaml.cs
@@ -19,8 +19,10 @@ namespace GSCFieldApp.Views;
 
 public partial class MapPage : ContentPage
 {
+    private CancellationTokenSource? gpsCancelation;
+    private bool _updateLocation = true;
 
-	public MapPage()
+    public MapPage()
 	{
 		InitializeComponent();
 
@@ -33,6 +35,89 @@ public partial class MapPage : ContentPage
 
         mapView.Map = mapControl.Map;
 
+        StartGPS();
+
     }
+
+    [Obsolete]
+    public async void StartGPS()
+    {
+        try
+        {
+            this.gpsCancelation?.Dispose();
+            this.gpsCancelation = new CancellationTokenSource();
+
+            await Task.Run(async () =>
+            {
+                while (!gpsCancelation.IsCancellationRequested)
+                {
+                    var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+#if __MAUI__ // WORKAROUND for Preview 11 will be fixed in Preview 13 https://github.com/dotnet/maui/issues/3597
+                    if (Application.Current == null)
+                        return;
+
+                    await Application.Current.Dispatcher.DispatchAsync(async () =>
+                    {
+#else
+                    await Device.InvokeOnMainThreadAsync(async () =>
+                    {
+#endif
+                        var location = await Geolocation.GetLocationAsync(request, this.gpsCancelation.Token)
+                            .ConfigureAwait(false);
+                        if (location != null)
+                        {
+                            MyLocationPositionChanged(location);
+                        }
+                    }).ConfigureAwait(false);
+
+                    await Task.Delay(200).ConfigureAwait(false);
+                }
+            }, gpsCancelation.Token).ConfigureAwait(false);
+        }
+        catch (Exception e)
+        {
+            Mapsui.Logging.Logger.Log(LogLevel.Error, e.Message, e);
+        }
+    }
+
+    public void StopGPS()
+    {
+        this.gpsCancelation?.Cancel();
+    }
+
+    /// <summary>
+    /// New informations from Geolocator arrived
+    /// </summary>        
+    /// <param name="e">Event arguments for new position</param>
+    [SuppressMessage("Usage", "VSTHRD100:Avoid async void methods")]
+    private async void MyLocationPositionChanged(Location e)
+    {
+        try
+        {
+            // check if I should update location
+            if (!_updateLocation)
+                return;
+
+            await Application.Current?.Dispatcher?.DispatchAsync(() =>
+            {
+                mapView?.MyLocationLayer.UpdateMyLocation(new Position(e.Latitude, e.Longitude));
+                if (e.Course != null)
+                {
+                    mapView?.MyLocationLayer.UpdateMyDirection(e.Course.Value, mapView?.Map.Navigator.Viewport.Rotation ?? 0);
+                }
+
+                if (e.Speed != null)
+                {
+                    mapView?.MyLocationLayer.UpdateMySpeed(e.Speed.Value);
+                }
+
+            })!;
+        }
+        catch (Exception ex)
+        {
+            Logger.Log(LogLevel.Error, ex.Message, ex);
+        }
+    }
+
 
 }
