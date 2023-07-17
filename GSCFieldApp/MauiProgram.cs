d0e8249fb75daad1d6eb370514c8d61ef3bf9b9c
gabriel.huot-vézina <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2023-07-17 11:50:42 -0400

    Added material design fonts and added icons in map page and menu.

--- GSCFieldApp/MauiProgram.cs
+++ GSCFieldApp/MauiProgram.cs
@@ -20,6 +20,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("MaterialDesignIconsDesktop.ttf", "MatDesign");
             });
 
 		// Need to add these to actually create them on start
---
+++ GSCFieldApp/Views/StationPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace GSCFieldApp.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
