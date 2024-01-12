a37e9b3ea8674397e3d2ae6cd4ea4f3886f9cbb5
gabriel.huot-vézina <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2024-01-12 13:08:57 -0500

    removed deleted button on stations and osm layers.

---
+++ GSCFieldApp/Converters/VisibilityFromILayer.cs
@@ -0,0 +1,36 @@
+﻿using GSCFieldApp.Dictionaries;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSCFieldApp.Converters
+{
+    public class VisibilityFromILayer: IValueConverter
+    {
+        public VisibilityFromILayer()
+        {
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && value.ToString() != ApplicationLiterals.aliasStations && 
+                value.ToString() != ApplicationLiterals.aliasOSM)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return "";
+        }
+    }
+}
