e5bb454dc6f37c930740cf3e037f9159479afb93
ghuotvez <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2025-01-22 09:15:13 -0500

    Adding stencil font to make it work with the svg images. Still not working.

--- GSCFieldApp/MauiProgram.cs
+++ GSCFieldApp/MauiProgram.cs
@@ -30,6 +30,7 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("MaterialDesignIconsDesktop.ttf", "MatDesign");
+                fonts.AddFont("STENCIL.TTF", "Stencil");
             });
 
 #if WINDOWS
