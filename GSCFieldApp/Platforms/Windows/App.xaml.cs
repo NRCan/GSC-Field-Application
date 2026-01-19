8b05cb5be6c01088ac4deba6b4813b5b3bff0144
jjoseph <jjoseph@AIT200910>
2026-01-19 11:31:01 -0800

    #488:  Now you can add a photo in the waypoint form.  It works from my end for both windows and android.

--- GSCFieldApp/Platforms/Windows/App.xaml.cs
+++ GSCFieldApp/Platforms/Windows/App.xaml.cs
@@ -17,6 +17,16 @@ public partial class App : MauiWinUIApplication
 	public App()
 	{
 		this.InitializeComponent();
+		
+#if DEBUG && !DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
+            UnhandledException += (sender, e) =>
+            {
+                if (global::System.Diagnostics.Debugger.IsAttached)
+                {
+                    global::System.Diagnostics.Debugger.Break();
+                }
+            };
+#endif
 	}
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
