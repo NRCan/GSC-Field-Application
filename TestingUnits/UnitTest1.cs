7464c6b569c57d281ab580ebc1110a26d7765a8b
ghuotvez <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2026-03-11 14:12:42 -0400

    A year later, again with testing units with xunit

---
+++ TestingUnits/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace TestingUnits
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            var expectedVersion = "3.1";
+
+            // Act
+            var actualVersion = "3.1";
+
+            // Assert
+            Assert.Equal(expectedVersion, actualVersion);
+        }
+    }
+}
