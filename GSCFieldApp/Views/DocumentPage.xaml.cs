85706f0d9c0ef923b322107de3d13cccc8558684
ghuotvez <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2026-04-02 09:57:05 -0400

    #521 small change in the way the picklist are filled on reload.

--- GSCFieldApp/Views/DocumentPage.xaml.cs
+++ GSCFieldApp/Views/DocumentPage.xaml.cs
@@ -46,8 +46,8 @@ public partial class DocumentPage : ContentPage
                 //After binding context is setup fill pickers
                 if (vm2 != null && vm2.SaveCommand.ExecutionTask == null && vm2.SaveStayCommand.ExecutionTask == null)
                 {
-                    await Task.Run(async () => await vm2.FillPickers());
-                    await Task.Run(async () => await vm2.InitModel());
+                    await vm2.FillPickers();
+                    await vm2.InitModel();
                     await vm2.Load(); //In case it is coming from an existing record in field notes
                 }
             }
