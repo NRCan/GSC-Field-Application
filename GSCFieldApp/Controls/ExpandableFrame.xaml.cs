8a48c455c5d073ae09ceb7e7371631828a784584
ghuotvez <gabriel.huot-vezina@nrcan-rncan.gc.ca>
2026-04-21 16:33:55 -0400

    #548 new chevron added to expand frame control and show if the frame is actually open or closed.

--- GSCFieldApp/Controls/ExpandableFrame.xaml.cs
+++ GSCFieldApp/Controls/ExpandableFrame.xaml.cs
@@ -27,6 +27,9 @@ public partial class ExpandableFrame : ContentView
     public static readonly BindableProperty IInfoAlertParameterProperty =
         BindableProperty.Create(nameof(InfoAlertParameter), typeof(string), typeof(ExpandableFrame), "");
 
+    public static readonly BindableProperty ChevronIconProperty =
+    BindableProperty.Create(nameof(ChevronIcon), typeof(string), typeof(ExpandableFrame), "&#xF0140;");
+
     public Color FrameColor
     {
         get => (Color)GetValue(FrameColorProperty);
@@ -57,6 +60,12 @@ public partial class ExpandableFrame : ContentView
         set => SetValue(IInfoAlertParameterProperty, value);
     }
 
+    public string ChevronIcon
+    {
+        get => (string)GetValue(ChevronIconProperty);
+        set => SetValue(ChevronIconProperty, value);
+    }
+
     /// <summary>
     /// Hide command to hide group of controls
     /// </summary>
@@ -66,6 +75,7 @@ public partial class ExpandableFrame : ContentView
     {
         // Reverse
         FrameContentVisibility = FrameContentVisibility ? false : true;
+        ChevronIcon = FrameContentVisibility ? "&#xF0140;" : "&#xF0143;";
 
     }
 
